77684c4a84a899253b19dd56e2c795faabc157e1
chottedifs <husadif.aldi@gmail.com>
2021-12-16 22:17:15 +0700

    seeder adminUser, camp, campBenefit

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title'=> 'User Interface Design',
+                'slug' => 'UI-design',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ],
+            [
+                'title'=> 'Started Learn',
+                'slug' => 'started-learn',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ],
+        ];
+
+        Camp::insert($camps);
+    }
+}
