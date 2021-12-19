1a802413014046d4eaa1f55055c890ed80b921a2
chottedifs <husadif.aldi@gmail.com>
2021-12-19 22:11:01 +0700

    install and integrate socialite

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Auth;
+
+class UserController extends Controller
+{
+    public function login(){
+        return view('auth.user.login');
+    }
+
+    public function google(){
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleProviderCallback(){        
+        $callBack = Socialite::driver('google')->stateless()->user();
+        $data = [
+            'name' => $callBack->getName(),
+            'email' => $callBack->getEmail(),
+            'avatar' => $callBack->getAvatar(),
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+        ];
+
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        Auth::login($user, true);
+
+        return redirect(route('welcome'));
+    }
+}
